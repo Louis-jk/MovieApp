bd7cf0ed46cf85a411692362bc0e1dcd5e21f362
JUNHO KIM <lippoint.surf0622@gmail.com>
2020-05-30 16:48:02 +0900

    불필요한 코드 삭제

--- src/components/MovieListDefault.js
+++ src/components/MovieListDefault.js
@@ -9,10 +9,7 @@ function MovieList({ likedMovies, movie, imgPath, onSetLike, genresList }) {
             <img className="card-img-top" src={(movie.poster_path)? `${imgPath}/w780${movie.poster_path}` : "noimg.jpg"} alt={movie.title} title={movie.title} />
             <div className="card-body px-0 px-md-3">
                 <h5 className="card-title">{movie.title}</h5>
-                <p className="card-text">평점 : {movie.vote_average}</p>            
-                {/* <p>장르테스트 : { console.log(result = _.filter(genresList, g => g.id === 18)) }</p>   
-                <p>{result.name}</p>
-                {console.log(movie.genre_ids.map(genreId => genreId))}    */}
+                <p className="card-text">평점 : {movie.vote_average}</p>   
                 <p>장르 : {movie.genre_ids.map(genreId => genreId).join(', ')}</p>                            
                 <p>개봉일자 : {movie.release_date}</p>   
                 <Link to={`/details/${movie.id}`} className="btn border-light float-right liked">자세히 보기</Link>
